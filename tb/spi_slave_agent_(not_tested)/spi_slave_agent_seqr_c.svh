5d69d447f593d898112baec09f801803d29c0c7b
AliMaher15 <ali.alimaher27@gmail.com>
2024-01-06 16:17:04 +0200

    figured out I cant fit both spi master and slave in the same env, so I renamed the folders to separete them

--- tb/spi_slave_agent_(not_tested)/spi_slave_agent_seqr_c.svh
+++
@@ -1,9 +0,0 @@
-class axi_slave_agent_seqr_c extends uvm_sequencer #(axi_item_c);
-
-    `uvm_component_utils(axi_slave_agent_seqr_c)
-    
-    function new(string name, uvm_component parent);
-        super.new(name,parent);
-    endfunction
-
-endclass : axi_slave_agent_seqr_c
